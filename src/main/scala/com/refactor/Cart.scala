11fee97e5f013e486944a4a4565cc1b54539012d
Michelle Austin <michelle.austin@itv.com>
2020-08-07 13:22:09 +0100

    refactored cart price calculator and added a unit test

--- src/main/scala/com/refactor/Cart.scala
+++ src/main/scala/com/refactor/Cart.scala
@@ -2,51 +2,9 @@ package com.refactor
 
 case class Cart(customer: Customer) {
 
-  //pricing
-  val REGULAR_PRICE = 2.0
-  val NEW_RELEASE_PRICE = 3.0
-  val CHILDRENS_PRICE = 1.5
-
-  //discounts
-  val REGULAR_DISCOUNT_THRESHOLD = 2
-  val REGULAR_DISCOUNT_MULTIPLIER = 1.5
-  val CHILDRENS_DISCOUNT_THRESHOLD = 3
-  val CHILDRENS_DISCOUNT_MULTIPLIER = 1.5
 
   def calculateTotal(): Double = {
-    customer.rentals
-      .map(_.movie.priceCode)
-      .map {
-        case p @ Regular    => regularSubtotal(p)
-        case p @ NewRelease => newReleaseSubtotal(p)
-        case p @ Childrens  => childrensSubtotal(p)
-      }
-      .sum
-  }
-
-  def regularSubtotal(code: PriceCode): Double = {
-    customer.rentals
-      .map(_.daysRented)
-      .map { d =>
-        if (d > REGULAR_DISCOUNT_THRESHOLD)
-          (d - REGULAR_DISCOUNT_THRESHOLD) * REGULAR_DISCOUNT_MULTIPLIER + REGULAR_PRICE
-        else REGULAR_PRICE
-      }
-      .sum
-  }
-
-  def newReleaseSubtotal(code: PriceCode): Double = {
-    customer.rentals.map(_.daysRented).map(_ * NEW_RELEASE_PRICE).sum
-  }
-
-  def childrensSubtotal(code: PriceCode): Double = {
-    customer.rentals
-      .map(_.daysRented)
-      .map { d =>
-        if (d > CHILDRENS_DISCOUNT_THRESHOLD)
-          (d - CHILDRENS_DISCOUNT_THRESHOLD) * CHILDRENS_DISCOUNT_MULTIPLIER + CHILDRENS_PRICE
-        else CHILDRENS_PRICE
-      }
-      .sum
+    customer.rentals.map(r => MoviePrices.computePriceCode(r))
+    .sum
   }
 }
--- src/main/scala/com/refactor/Movie.scala
+++ src/main/scala/com/refactor/Movie.scala
@@ -16,3 +16,40 @@ case class Customer(name: String, rentals: List[Rental]) {
   }
 }
 
+object MoviePrices {
+
+  //pricing
+  val REGULAR_PRICE = 2.0
+  val NEW_RELEASE_PRICE = 3.0
+  val CHILDRENS_PRICE = 1.5
+
+  //discounts
+  val REGULAR_DISCOUNT_THRESHOLD = 2
+  val REGULAR_DISCOUNT_MULTIPLIER = 1.5
+  val CHILDRENS_DISCOUNT_THRESHOLD = 3
+  val CHILDRENS_DISCOUNT_MULTIPLIER = 1.5
+
+  def computePriceCode(rental: Rental): Double = {
+    rental.movie.priceCode match {
+      case Regular    => regularSubtotal(rental.daysRented)
+      case NewRelease => newReleaseSubtotal(rental.daysRented)
+      case Childrens  => childrensSubtotal(rental.daysRented)
+    }
+  }
+
+  def regularSubtotal(daysRented: Int): Double = {
+    if (daysRented > REGULAR_DISCOUNT_THRESHOLD)
+      (daysRented - REGULAR_DISCOUNT_THRESHOLD) * REGULAR_DISCOUNT_MULTIPLIER + REGULAR_PRICE
+    else REGULAR_PRICE
+  }
+
+  def newReleaseSubtotal(daysRented: Int): Double = {
+    daysRented * NEW_RELEASE_PRICE
+  }
+
+  def childrensSubtotal(daysRented: Int): Double = {
+    if (daysRented > CHILDRENS_DISCOUNT_THRESHOLD)
+      (daysRented - CHILDRENS_DISCOUNT_THRESHOLD) * CHILDRENS_DISCOUNT_MULTIPLIER + CHILDRENS_PRICE
+    else CHILDRENS_PRICE
+  }
+}
